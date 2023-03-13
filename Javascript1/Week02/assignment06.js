58a16fb9a2b7005adccacf267bbe05e2b59b04a8
Alpcan Uçkan <90960057+auckan@users.noreply.github.com>
2023-03-13 22:39:56 +0100

    Delete assignment06.js

--- Javascript1/Week02/assignment06.js
+++
@@ -1,8 +0,0 @@
-//declare array and add a new element to it
-let favFruits = [""];
-console.log("The value of favFruits will be: []");
-console.log(favFruits);
-let favAnimals = ["cat", "dog", "lion", "squirrel"];
-console.log(favAnimals);
-favAnimals.push = "baby pig";
-console.log(favAnimals);
