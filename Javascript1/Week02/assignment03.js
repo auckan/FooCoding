c54f092ca6dba4cbbe302a55646e47ae491cafe5
Alpcan Uçkan <90960057+auckan@users.noreply.github.com>
2023-03-13 22:39:40 +0100

    Delete assignment03.js

--- Javascript1/Week02/assignment03.js
+++
@@ -1,7 +0,0 @@
-//declare and initilize a number type variable
-let x;
-console.log("The value of x will be: undefined");
-console.log(x);
-x = 54;
-console.log("The value of x will be: 54");
-console.log(x);
